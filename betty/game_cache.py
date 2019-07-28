6f27f2d4c5a2806b2e02ec01b04cdf9670b774ba
TouwaStar <silaczewski.mateusz@gmail.com>
2019-07-28 08:14:32 +0200

    possible list of reference ids to match

--- betty/game_cache.py
+++ betty/game_cache.py
@@ -20,7 +20,7 @@ product_cache = {
         "free_to_play": False
     },
     "Call of Cthulhu: Dark Corners of the Earth": {
-        "reference_id": "CA1CSTPCBG01",
+        "reference_id": ["CA1CSTPCBG01"],
         "external_reference_id": "CA1CSTPCBG01",
         "uuid": "85956f995991b79fa7c1b820fa531b57c6068999",
         "local_id": "35",
@@ -43,7 +43,7 @@ product_cache = {
         "free_to_play": True
     },
     "Dishonored 2": {
-        "reference_id": "DI2CSTPCBG01",
+        "reference_id": ["DI2CSTPCBG01"],
         "external_reference_id": "DI2CSTPCBG01",
         "uuid": "e38115c843df43eedb85c1fd27e83c4057690143",
         "local_id": "18",
@@ -52,7 +52,7 @@ product_cache = {
         "free_to_play": False
     },
     "Dishonored: Death of the Outsider": {
-        "reference_id": "DI2A01PCBG01",
+        "reference_id": ["DI2A01PCBG01"],
         "external_reference_id": "DI2A01PCBG01",
         "uuid": "c97b8697928cdd5798e6e35663d9d6664efdf51d",
         "local_id": "19",
@@ -61,7 +61,7 @@ product_cache = {
         "free_to_play": False
     },
     "DOOM 3: BFG Edition": {
-        "reference_id": "DO3CBFPCBG01",
+        "reference_id": ["DO3CBFPCBG01"],
         "external_reference_id": "DO3CBFPCBG01",
         "uuid": "2f98de5b58f15dc2cb0e2908b9cb7744571c0cba",
         "local_id": "40",
@@ -70,7 +70,7 @@ product_cache = {
         "free_to_play": False
     },
     "Doom Eternal": {
-        "reference_id": "DO5CS1PCBG01BASE",
+        "reference_id": ["DO5CS1PCBG01BASE"],
         "external_reference_id": "DO5CS1PCBG01BASE",
         "uuid": "bb93e7936db5daf4d99f66fbfee74535ca499bb6",
         "local_id": "51",
@@ -80,7 +80,7 @@ product_cache = {
     },
     "DOOM II": {
         "reference_id": "DO2CSTPCBG01",
-        "external_reference_id": "DO2CSTPCBG01",
+        "external_reference_id": ["DO2CSTPCBG01"],
         "uuid": "82d35ffe87b961d0c0f170190e52e6cf632b12e5",
         "local_id": "25",
         "owned": False,
@@ -88,7 +88,7 @@ product_cache = {
         "free_to_play": False
     },
     "Fallout 2: A Post Nuclear Role Playing Game": {
-        "reference_id": "FA2CSTPCBG01",
+        "reference_id": ["FA2CSTPCBG01"],
         "external_reference_id": "FA2CSTPCBG01",
         "uuid": "1b336a1776324a850272f3864d547a907e84e824",
         "local_id": "22",
@@ -104,7 +104,7 @@ product_cache = {
         "free_to_play": False
     },
     "Fallout 76": {
-        "reference_id": "FAOCS1PCBG01BASE",
+        "reference_id": ["FAOCS1PCBG01BASE"],
         "external_reference_id": "FAOCS1PCBG01BASE",
         "uuid": "4bde4ecb7ca16b3408445f136c6cde4f11263f8c",
         "local_id": "20",
@@ -127,7 +127,7 @@ product_cache = {
         "free_to_play": True
     },
     "Fallout Tactics: Brotherhood of Steel": {
-        "reference_id": "FATCSTPCBG01",
+        "reference_id": ["FATCSTPCBG01"],
         "external_reference_id": "FATCSTPCBG01",
         "uuid": "bc388f05164f095cbe171ffbf16f0966aa9e5f6e",
         "local_id": "23",
@@ -136,7 +136,7 @@ product_cache = {
         "free_to_play": False
     },
     "Fallout: A Post Nuclear Role Playing Game": {
-        "reference_id": "FA1CSTPCBG01",
+        "reference_id": ["FA1CSTPCBG01"],
         "external_reference_id": "FA1CSTPCBG01",
         "uuid": "ef9d01c83c0c2e2a7cec3a147978827e3c4ecfc4",
         "local_id": "21",
@@ -152,7 +152,7 @@ product_cache = {
         "free_to_play": False
     },
     "Quake": {
-        "reference_id": "QU1CSTPCBG01",
+        "reference_id": ["QU1CSTPCBG01"],
         "external_reference_id": "QU1CSTPCBG01",
         "uuid": "c8c24e10c5fe1b43ce63a89d5d0b7e39f2b213d0",
         "local_id": "36",
@@ -161,7 +161,7 @@ product_cache = {
         "free_to_play": False
     },
     "Quake Champions": {
-        "reference_id": "QCACP01",
+        "reference_id": ["QCACP01"],
         "uuid": "3bfbe957f785b1db6bf9db2b9aee9cc7c4358945",
         "local_id": "11",
         "owned": True,
@@ -176,7 +176,7 @@ product_cache = {
         "free_to_play": True
     },
     "Quake II": {
-        "reference_id": "QU2CSTPCBG01",
+        "reference_id": ["QU2CSTPCBG01"],
         "external_reference_id": "QU2CSTPCBG01",
         "uuid": "ab68736f325dca93dd5721d319446d2f0c45f11e",
         "local_id": "37",
@@ -185,7 +185,7 @@ product_cache = {
         "free_to_play": False
     },
     "Quake III Arena": {
-        "reference_id": "QU3CSTPCBG01",
+        "reference_id": ["QU3CSTPCBG01"],
         "external_reference_id": "QU3CSTPCBG01",
         "uuid": "0da47b5a3badb925d6ecb3e9757dee5fec965fa8",
         "local_id": "38",
@@ -194,7 +194,7 @@ product_cache = {
         "free_to_play": False
     },
     "RAGE 2": {
-        "reference_id": "RA2CDXPCBG01BASE",
+        "reference_id": ["RA2CDXPCBG01BASE","RA2CSTPCBG01"],
         "external_reference_id": "RA2CDXPCBG01BASE",
         "uuid": "2c86dc72c7b1b37313d587cdce1f54c36aadc277",
         "local_id": "45",
@@ -203,7 +203,7 @@ product_cache = {
         "free_to_play": False
     },
     "RAGE 2 Test": {
-        "reference_id": "RA2CDXPCBG01BASE",
+        "reference_id": ["RA2CDXPCBG01BASE"],
         "external_reference_id": "RA2CDXPCBG01BASE",
         "uuid": "34aa672d479fc960e19be1357bd1049f79618b20",
         "local_id": "47",
@@ -212,7 +212,7 @@ product_cache = {
         "free_to_play": False
     },
     "Return to Castle Wolfenstein": {
-        "reference_id": "WO2CSTPCBG01 ",
+        "reference_id": ["WO2CSTPCBG01"],
         "uuid": "11614101775dabfb10e3ce8420ceabf8c12a0a05",
         "local_id": "27",
         "owned": False,
@@ -220,7 +220,7 @@ product_cache = {
         "free_to_play": False
     },
     "The Elder Scrolls III: Morrowind Game of the Year Edition": {
-        "reference_id": "EL3GSTPCBG01",
+        "reference_id": ["EL3GSTPCBG01"],
         "external_reference_id": "EL3GSTPCBG01",
         "uuid": "6efe5da31f825a7bca2231b030106c7178e0d422",
         "local_id": "31",
@@ -229,7 +229,7 @@ product_cache = {
         "free_to_play": False
     },
     "The Elder Scrolls IV: Oblivion Game of the Year Edition": {
-        "reference_id": "EL4GSTPCBG01",
+        "reference_id": ["EL4GSTPCBG01"],
         "external_reference_id": "EL4GSTPCBG01",
         "uuid": "18b79e6ded89e590e7ddca53f5f7883166ad17bc",
         "local_id": "32",
@@ -259,7 +259,7 @@ product_cache = {
         "free_to_play": True
     },
     "The Evil Within 2": {
-        "reference_id": "EV2CSTPCBG01",
+        "reference_id": ["EV2CSTPCBG01"],
         "external_reference_id": "EV2CSTPCBG01",
         "uuid": "2a1d18ff555b203ee9df11a7f13ce337e9a9573b",
         "local_id": "16",
@@ -268,7 +268,7 @@ product_cache = {
         "free_to_play": False
     },
     "The Evil Within Digital Deluxe": {
-        "reference_id": "EV1CSPPCBG01",
+        "reference_id": ["EV1CSPPCBG01"],
         "external_reference_id": "EV1CSPPCBG01",
         "uuid": "e8c03760e47866ffd1962a9ed58e65a831847c13",
         "local_id": "17",
@@ -284,7 +284,7 @@ product_cache = {
         "free_to_play": False
     },
     "Ultimate DOOM": {
-        "reference_id": "DO1CCPPCBG01",
+        "reference_id": ["DO1CCPPCBG01"],
         "external_reference_id": "DO1CCPPCBG01",
         "uuid": "7db39a7a6e21810ec5f0539b150d8aede96abe7a",
         "local_id": "24",
@@ -293,7 +293,7 @@ product_cache = {
         "free_to_play": False
     },
     "Wolfenstein 3D": {
-        "reference_id": "WO1CSTPCBG01",
+        "reference_id": ["WO1CSTPCBG01"],
         "external_reference_id": "WO1CSTPCBG01",
         "uuid": "44f9807bfafd93585249affcd659a7cb383f69b5",
         "local_id": "26",
@@ -302,7 +302,7 @@ product_cache = {
         "free_to_play": False
     },
     "Wolfenstein II: The New Colossus": {
-        "reference_id": "WO5CSTPCBG01",
+        "reference_id": ["WO5CSTPCBG01"],
         "external_reference_id": "WO5CSTPCBG01",
         "uuid": "88a9b52105998cad2e0fb8491e50701a00e597cd",
         "local_id": "15",
@@ -311,7 +311,7 @@ product_cache = {
         "free_to_play": False
     },
     "Wolfenstein: Youngblood DE": {
-        "reference_id": "WOYCD1PCBGERBASE",
+        "reference_id": ["WOYCD1PCBGERBASE"],
         "uuid": "a327b70a48efc15ff5ed3855e48e30a96d93b98b",
         "local_id": "50",
         "owned": False,
@@ -319,7 +319,7 @@ product_cache = {
         "free_to_play": False
     },
     "Wolfenstein: Youngblood": {
-        "reference_id": "WOYCD1PCBG01BASE",
+        "reference_id": ["WOYCD1PCBG01BASE"],
         "external_reference_id": "WOYCD1PCBG01BASE",
         "uuid": "c91af732db5fd0b75f0114147af1bb431dc10d55",
         "local_id": "49",
